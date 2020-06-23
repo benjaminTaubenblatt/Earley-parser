4e40d34cc5297494c532badf8a9f5226d0eb4688
Benjamin Taubenblatt <BenjaminMT64@gmail.com>
2020-06-23 12:11:15 -0400

    made progress on scanner

--- earley.py
+++ earley.py
@@ -1,22 +1,25 @@
-from typing import List 
+from typing import List
+
 
 class State:
-    def __init__(self, sid, rule, origin, dot):
+    def __init__(self, sid, rule, origin, dot, children=[]):
         self.sid = sid
         self.rule = rule
         self.origin = origin
         self.dot = dot
-        self.prev = []
-        self.is_finished = False 
-    
+        self.children = children
+
+
     def __str__(self):
         return (f'sid={self.sid}, rule={self.rule}, origin={self.origin}, '
-                f'dot={self.dot}, prev={self.prev}, is_finished={self.is_finished}')
+                f'dot={self.dot}, children={self.children}')
+
 
 class Rule:
     def __init__(self, lhs, rhs):
         self.lhs = lhs
         self.rhs = rhs
+
 
     def __str__(self):
         return f'Rule(lhs={self.lhs}, rhs={self.rhs})'
@@ -27,40 +30,64 @@ class EarleyParser:
         self.chart = [[]]
         self.grammar = grammar
         self.terminals = terminals
-        self.counter = 0
-    
-    def print_chart(self):
+        self.counter = -1
+
+
+    def print_chart(self) -> None:
         for i in range(len(self.chart)):
             for j in range(len(self.chart[i])):
-                print(f'row={i}, column={j}, state=State({str(self.chart[i][j])})')
+                print(
+                    f'row={i}, column={j}, state=State({str(self.chart[i][j])})'
+                )
 
-    def predictor(self, state: State, k: int):
+
+    def get_counter(self) -> int:
+        self.counter += 1
+        return self.counter
+
+
+    def is_finished(self, state: State, words: List[str]) -> bool:
+        return state.dot == len(words)
+
+
+    def predictor(self, state: State, k: int) -> None:
         B = state.rule.rhs[state.dot]
         for rhs in self.grammar[B]:
             self.chart[k].append(
-                State(sid=f'S{self.counter}',
+                State(sid=f'S{self.get_counter()}',
                       rule=Rule(lhs=B, rhs=rhs),
                       origin=state.origin,
                       dot=state.dot))
 
-    def parse(self, words: List[str]):
+
+    def scanner(self, state: State, k: int, words: List[str]) -> None:
+        if words[k] in self.grammar[state.rule.lhs]:
+            # if input at pos k subset of POS for current terminal in rule
+            self.chart.append([])
+            self.chart[k + 1].append(
+                State(sid=f'S{self.get_counter()}',
+                      rule=Rule(lhs=state.rule.rhs[state.dot], rhs=words[k]),
+                      origin=state.origin,
+                      dot=state.dot + 1,
+                      children=state.children))
+
+
+    def parse(self, words: List[str]) -> List[List[State]]:
         self.chart[0].append(
-            State(sid=f'S{self.counter}',
+            State(sid=f'S{self.get_counter()}',
                   rule=Rule('GAMMA', ['S']),
                   origin=0,
                   dot=0))
         for k in range(len(words)):
             for state in self.chart[k]:
-                if not state.is_finished:
+                if not self.is_finished(state, words):
                     if state.rule.rhs[state.dot] not in terminals:
                         # non-terminal
                         self.predictor(state, k)
-                        self.print_chart()
-                        return
+                        # self.print_chart()
                     else:
                         # terminal
-                        # scanner
-                        pass
+                        self.scanner(state, k, words)
                 else:
                     # completer
                     pass
@@ -72,12 +99,12 @@ grammar = {
     'PP': [['P', 'NP']],
     'VP': [['V', 'NP'], ['VP', 'PP']],
     'NP': [['NP', 'PP'], ['N']],
-    'N': [['astronomers'], ['ears'], ['stars'], ['telescopes']],
-    'V': [['saw']],
-    'P': [['with']]
+    'N': ['astronomers', 'ears', 'stars', 'telescopes'],
+    'V': ['saw'],
+    'P': ['with']
 }
-terminals = {'astronomers', 'ears', 'stars', 'telescopes', 'saw', 'with'}
+terminals = {'N', 'V', 'P'}
 
-words = ['astronomers', 'saw', 'starts', 'with', 'ears']
+words = ['astronomers', 'saw', 'stars', 'with', 'ears']
 earley = EarleyParser(grammar, terminals)
 earley.parse(words)
