8ffc4500819f97505e32f1b82d349cd45f35f4e0
Benjamin Taubenblatt <BenjaminMT64@gmail.com>
2020-06-23 16:08:50 -0400

    added generator for sid, added completer with bugs

--- earley.py
+++ earley.py
@@ -1,18 +1,20 @@
-from typing import List
+from typing import List, Iterator
 
 
 class State:
-    def __init__(self, sid, rule, origin, dot, children=[]):
+    def __init__(self, sid, rule, origin, dot, children=[], operation=""):
         self.sid = sid
         self.rule = rule
         self.origin = origin
         self.dot = dot
         self.children = children
-
+        self.operation = operation
 
     def __str__(self):
-        return (f'sid={self.sid}, rule={self.rule}, origin={self.origin}, '
-                f'dot={self.dot}, children={self.children}')
+        return (
+            f"sid={self.sid}, rule={self.rule}, origin={self.origin}, "
+            f"dot={self.dot}, children={self.children}, operation={self.operation}"
+        )
 
 
 class Rule:
@@ -20,9 +22,8 @@ class Rule:
         self.lhs = lhs
         self.rhs = rhs
 
-
     def __str__(self):
-        return f'Rule(lhs={self.lhs}, rhs={self.rhs})'
+        return f"Rule(lhs={self.lhs}, rhs={self.rhs})"
 
 
 class EarleyParser:
@@ -30,57 +31,68 @@ class EarleyParser:
         self.chart = [[]]
         self.grammar = grammar
         self.terminals = terminals
-        self.counter = -1
-
+        self.sid_gen = self.generate_sid()
 
     def print_chart(self) -> None:
         for i in range(len(self.chart)):
             for j in range(len(self.chart[i])):
                 print(
-                    f'row={i}, column={j}, state=State({str(self.chart[i][j])})'
+                    f"row={i}, column={j}, state=State({str(self.chart[i][j])})"
                 )
 
-
-    def get_counter(self) -> int:
-        self.counter += 1
-        return self.counter
-
+    def generate_sid(self) -> Iterator[str]:
+        n = 0
+        while True:
+            yield f"S{n}"
+            n += 1
 
     def is_finished(self, state: State, words: List[str]) -> bool:
         return state.dot == len(words)
-
 
     def predictor(self, state: State, k: int) -> None:
         B = state.rule.rhs[state.dot]
         for rhs in self.grammar[B]:
             self.chart[k].append(
-                State(sid=f'S{self.get_counter()}',
+                State(sid=next(self.sid_gen),
                       rule=Rule(lhs=B, rhs=rhs),
                       origin=state.origin,
-                      dot=state.dot))
-
+                      dot=state.dot,
+                      operation="predictor"))
 
     def scanner(self, state: State, k: int, words: List[str]) -> None:
         if words[k] in self.grammar[state.rule.rhs[state.dot]]:
             # if input at pos k subset of POS for current terminal in rule
             self.chart.append([])
             self.chart[k + 1].append(
-                State(sid=f'S{self.get_counter()}',
+                State(sid=next(self.sid_gen),
                       rule=Rule(lhs=state.rule.rhs[state.dot], rhs=words[k]),
                       origin=state.origin,
                       dot=state.dot + 1,
-                      children=state.children))
+                      children=state.children,
+                      operation="scanner"))
 
+    def completer(self, state: State, k: int) -> None:
+        for st in self.chart[state.origin]:
+            self.chart[k].append(
+                State(sid=next(self.sid_gen),
+                      rule=Rule(lhs=st.rule.lhs, rhs=st.rule.rhs),
+                      origin=st.origin,
+                      dot=st.dot + 1,
+                      children=[state],
+                      operation="completer"))
 
     def parse(self, words: List[str]) -> List[List[State]]:
+        seen = set()
         self.chart[0].append(
-            State(sid=f'S{self.get_counter()}',
+            State(sid=next(self.sid_gen),
                   rule=Rule('GAMMA', ['S']),
                   origin=0,
-                  dot=0))
+                  dot=0,
+                  operation="seed"))
         for k in range(len(words)):
             for state in self.chart[k]:
-                if not self.is_finished(state, words):
+                if not self.is_finished(state, words) and state.rule.rhs[
+                        state.dot] not in seen:
                     if state.rule.rhs[state.dot] not in terminals:
                         # non-terminal
                         self.predictor(state, k)
@@ -93,17 +105,19 @@ class EarleyParser:
                         print("scanner")
                         self.print_chart()
                         print()
-                        return
+                    seen.add(state.rule.rhs[state.dot])
                 else:
                     # completer
-                    pass
+                    self.completer(state, k)
+                    print("completer")
+                    self.print_chart()
         return self.chart
+
 
 """
 TODO: 
     1. check repeats in chart
 """
-
 
 grammar = {
     'S': [['NP', 'VP']],
