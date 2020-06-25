47e766ed1effa7002fd9c337d8dba99e7a7c940c
Benjamin Taubenblatt <BenjaminMT64@gmail.com>
2020-06-25 18:07:25 -0400

    cleaned/refactored code, debugged completor, added support for recovering parse forest

--- earley2.py
+++ earley2.py
@@ -2,15 +2,24 @@ from typing import List, Iterator
 
 
 class State:
-    def __init__(self, sid, rule, position, pointers=[], operation="") -> None:
-        self.sid = sid
+    def __init__(self, rule, position, pointers=[], operation="") -> None:
+        self.sid = None 
         self.rule = rule
         self.position = position 
         self.pointers = pointers
         self.operation = operation
+    
 
+    def complete(self) -> bool:
+        if self.sid == 'S20':
+            print("incomp", self.position[0], len(self.rule.rhs), self.position[1])
+        return (self.position[0] + len(self.rule.rhs)) == self.position[1]
+    
     def next_category(self) -> str:
-        return self.rule.rhs[self.position[1] - self.position[0]]
+        rule_idx = self.position[1] - self.position[0]
+        if rule_idx < len(self.rule.rhs):
+            return self.rule.rhs[rule_idx]
+        return "" 
 
     def __str__(self) -> str:
         return (
@@ -40,7 +49,6 @@ class EarleyParser:
         self.grammar = grammar
         self.terminals = terminals
         self.sid_gen = self.generate_sid()
-        # self.seen = set()
 
     def init_chart(self, words: List[str]) -> None:
         for _ in range(len(words) + 1):
@@ -58,79 +66,106 @@ class EarleyParser:
             yield f"S{n}"
             n += 1
 
-    def incomplete(self, state: State) -> bool:
-        return (state.position[0] + len(state.rule.rhs)) != state.position[1]
 
     def enqueue(self, state: State, k: int) -> None:
         if state not in self.chart[k]:
+            state.sid = next(self.sid_gen)
             self.chart[k].append(state)
 
-    def predictor(self, state: State, k: int) -> None:
+    def predictor(self, state: State) -> None:
         B = state.next_category()
         j = state.position[1]
-        for rhs in self.grammar[B]:
-            self.enqueue(State(sid=next(self.sid_gen),
-                               rule=Rule(lhs=B, rhs=rhs),
+        for rhs in self.grammar.get(B, []):
+            self.enqueue(State(rule=Rule(lhs=B, rhs=rhs),
                                position=[j, j],
                                operation="predictor"), j)
 
-    def scanner(self, state: State, k: int, words: List[str]) -> None:
+    def scanner(self, state: State, words: List[str]) -> None:
         B = state.next_category()
         j = state.position[1]
-        if words[j] in self.grammar[B]:
-            # if input at pos k subset of POS for current terminal in rule
-            self.enqueue(State(sid=next(self.sid_gen),
-                               rule=Rule(lhs=B, rhs=[words[j]]),
-                               position=[j, j + 1],
-                               operation="scanner"), j + 1)
+        if j < len(words):
+            if words[j] in self.grammar.get(B, []):
+                # if input at pos k subset of POS for current terminal in rule
+                self.enqueue(State(rule=Rule(lhs=B, rhs=[words[j]]),
+                                   position=[j, j + 1],
+                                   operation="scanner"), j + 1)
 
-    def completer(self, state: State, k: int) -> None:
+    def completer(self, state: State) -> None:
         j = state.position[0]
         k = state.position[1]
         for st in self.chart[j]:
-            if self.incomplete(st):
-                if st.next_category() == state.rule.lhs:
-                    i = state.position[0]
-                    self.enqueue(State(sid=next(self.sid_gen),
-                                       rule=Rule(lhs=st.rule.lhs, rhs=st.rule.rhs),
-                                       position=[i, k],
-                                       pointers=st.pointers + [state.sid],
-                                       operation="completer"), k)
+            if st.next_category() == state.rule.lhs\
+                    and st.position[1] == j and st.rule.lhs != 'GAMMA':
+                i = st.position[0]
+                self.enqueue(State(rule=Rule(lhs=st.rule.lhs, rhs=st.rule.rhs),
+                                   position=[i, k],
+                                   pointers=st.pointers + [state.sid],
+                                   operation="completer"), k)
 
     def parse(self, words: List[str]) -> List[List[State]]:
         self.init_chart(words)
-        self.enqueue(State(sid=next(self.sid_gen),
-                           rule=Rule('GAMMA', ['S']),
+        self.enqueue(State(rule=Rule('GAMMA', ['S']),
                            position=[0, 0],
                            operation="seed"), 0)
         for k in range(len(words) + 1):
+            print(k)
             for state in self.chart[k]:
-                if self.incomplete(state):
-                    if state.next_category() not in self.terminals:
-                            # non-terminal
-                            self.predictor(state, k)
-                            print("predictor")
-                            self.print_chart()
-                            print()
-                    else:
-                        # terminal
-                        self.scanner(state, k, words)
-                        print("scanner")
+                if not state.complete() and state.next_category() not in self.terminals:
+                        # non-terminal
+                        self.predictor(state)
+                        print("predictor " + str(state.rule))
                         self.print_chart()
                         print()
-                        print(k)
+                elif not state.complete() and state.next_category() in self.terminals and k != len(words):
+                    # terminal
+                    self.scanner(state, words)
+                    print("scanner " + str(state.rule))
+                    self.print_chart()
+                    print()
                 else:
                     # completer
-                    self.completer(state, k)
-                    print("completer")
+                    self.completer(state)
+                    print("completer " + str(state.rule))
                     self.print_chart()
                     # return
+        # self.print_chart()
         return self.chart
+
+    def forest(self):
+        def find_child(sid):
+            for row in range(len(self.chart)-1, -1, -1):
+                for col in range(len(self.chart[row])-1, -1, -1):
+                    if self.chart[row][col].sid == sid:
+                        return self.chart[row][col]
+
+        def find_children(state):
+            current_tree.extend(['(', state.rule.lhs])
+            if state.rule.lhs in self.terminals:
+                current_tree.extend(['(', state.rule.rhs[0], ')'])
+            else:
+                for sid in state.pointers:
+                    child = find_child(sid) 
+                    find_children(child)
+                    current_tree.append(')')
+            
+        parse_forest = []
+        for st in self.chart[-1]:
+            if st.rule.lhs == 'S':
+                current_tree = []
+                find_children(st)
+                parse_forest.append(current_tree)
+        return parse_forest
+
+
+
 
 
 """
 TODO: 
-    1. check repeats in chart
+    1. debug completor, dot rules, complete method, next category
+    2. improve parse forest retrieval 
+        a. multiple parses on same 'S' in last entry of chart?
+
 """
 
 grammar = {
@@ -144,6 +179,10 @@ grammar = {
 }
 terminals = {'N', 'V', 'P'}
 
-words = ['astronomers', 'saw', 'stars'] #, 'with', 'ears']
+words = ['astronomers', 'saw', 'stars']#, 'with', 'ears']
 earley = EarleyParser(grammar, terminals)
 earley.parse(words)
+forest = earley.forest()
+for tree in forest:
+    print("".join(tree))
+
